de08414b68260f5657a773025f06375cea2dc9e7
anh-ht <anhht@tmh-techlab.vn>
2018-11-02 15:57:09 +0700

    rav_v2

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // configure body parser
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(loopback.token());
 
